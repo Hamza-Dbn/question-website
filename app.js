b5e176d6ada77cabc80a0f895af6b1761a414e6d
Hamza Darbani <76852492+Hamza-Dbn@users.noreply.github.com>
2022-08-20 16:05:47 +0200

    js code

---
+++ app.js
@@ -0,0 +1,11 @@
+//Using selectors inside the element
+//Traversing the Dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle("show-text");
+  });
+});
